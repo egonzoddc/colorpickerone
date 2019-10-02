93111b966ad9e8b7809975e12a817e6505dc26b3
Efren Gonzales <colorado3106@gmail.com>
2019-10-02 15:27:29 -0600

    built a color picker with sliders

---
+++ script.js
@@ -0,0 +1,34 @@
+window.onload = function() {
+	initColorPicker();
+};
+
+function initColorPicker() {
+	let colorBox = document.getElementById("color-box");
+	let rgb = {
+		red: document.getElementById("red")
+		green: document.getElementById("green")
+		blue: document.getElementById("blue")
+	};
+	let colorPickers = document.getElementsByClassName("picker")
+	setColorPickerEventListeners(colorBox, rgb. colorPickers);
+}
+
+function setColorPickerEventListeners(colorBox, rgb, pickerElements) {
+	let pickerLen = pickerElements.length;
+	for(let i = 0; i , < pickerLen; i++) {
+		pickerElements[i].addEventListener('change', () => {
+			console.log("Red value: ", rgb.red.value);
+			let red = rgb.red.value;
+			let green = rgb.green.value;
+			let blue = rgb.blue.value;
+			setBoxBGColor(colorBox, red, green, blue);
+		});
+	}
+}
+
+function setBoxBGColor(colorBox, red, green, blue) {
+	rgbVal = [red, green, blue].join(',');
+	colorBox.style.backgroundColor = "rgb(" + rgbval + ")";
+}
+
+	function setDisplayValues (red, green, blue)
