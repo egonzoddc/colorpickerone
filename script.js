7e005952004cde4a312fddcec616209011d51fb5
Efren Gonzales <colorado3106@gmail.com>
2019-10-02 23:24:46 -0600

    built a color picker with sliders

--- script.js
+++ script.js
@@ -5,30 +5,38 @@ window.onload = function() {
 function initColorPicker() {
 	let colorBox = document.getElementById("color-box");
 	let rgb = {
-		red: document.getElementById("red")
-		green: document.getElementById("green")
+		red: document.getElementById("red"),
+		green: document.getElementById("green"),
 		blue: document.getElementById("blue")
 	};
-	let colorPickers = document.getElementsByClassName("picker")
-	setColorPickerEventListeners(colorBox, rgb. colorPickers);
+	let colorPickers = document.getElementsByClassName("picker");
+	setColorPickerEventListeners(colorBox, rgb, colorPickers);
 }
 
-function setColorPickerEventListeners(colorBox, rgb, pickerElements) {
+function setColorPickerEventListeners(element, colors, pickerElements) {
 	let pickerLen = pickerElements.length;
-	for(let i = 0; i , < pickerLen; i++) {
+	for (let i = 0; i < pickerLen; i++) {
 		pickerElements[i].addEventListener('change', () => {
-			console.log("Red value: ", rgb.red.value);
-			let red = rgb.red.value;
-			let green = rgb.green.value;
-			let blue = rgb.blue.value;
-			setBoxBGColor(colorBox, red, green, blue);
+			let red = colors.red.value;
+			let green = colors.green.value;
+			let blue = colors.blue.value;
+			setElementBGColor(element, red, green, blue);
+			setDisplayValues(red, green, blue)
 		});
-	}
+		}
 }
 
-function setBoxBGColor(colorBox, red, green, blue) {
-	rgbVal = [red, green, blue].join(',');
-	colorBox.style.backgroundColor = "rgb(" + rgbval + ")";
+function setElementBGColor(bgElement, red, green, blue) {
+	let rgbVal = [red, green, blue].join(',');
+	bgElement.style.backgroundColor = "rgb(" + rgbVal + ")";
 }
 
-	function setDisplayValues (red, green, blue)
+function setDisplayValues(red, green, blue) {
+	let redVal = document.getElementById("redVal");
+	let greenVal = document.getElementById("greenVal");
+	let blueVal = document.getElementById("blueVal");
+
+	redVal.innerText = red;
+	greenVal.innerText = green;
+	blueVal.innerText = blue;
+}
